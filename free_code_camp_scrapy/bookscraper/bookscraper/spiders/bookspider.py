86197e70c2611b813a35bb582bbf886352adcb15
Hamza Ehsan Khan <khanh29@cardiff.ac.uk>
2024-06-28 10:55:59 +0100

    Part 5: Crawling With Scrapy

--- free_code_camp_scrapy/bookscraper/bookscraper/spiders/bookspider.py
+++ free_code_camp_scrapy/bookscraper/bookscraper/spiders/bookspider.py
@@ -36,11 +36,11 @@ class BookspiderSpider(scrapy.Spider):
             'stars' : product.css("p.star-rating").attrib['class'],
             'upc' : table_rows[0].css('td ::text').get(),
             'product_type' : table_rows[1].css('td ::text').get(),
-            'price_excl' : table_rows[2].css('td ::text').get(),
-            'price_incl' : table_rows[3].css('td ::text').get(),
-            'price_incl' : table_rows[4].css('td ::text').get(),
-            'price_incl' : table_rows[5].css('td ::text').get(),
-            'price_incl' : table_rows[6].css('td ::text').get(),
+            'price_excl_tax' : table_rows[2].css('td ::text').get(),
+            'price_incl_tax' : table_rows[3].css('td ::text').get(),
+            'tax' : table_rows[4].css('td ::text').get(),
+            'availability' : table_rows[5].css('td ::text').get(),
+            'num_reviews' : table_rows[6].css('td ::text').get(),
             
 
         }
