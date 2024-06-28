704180ecdfe1a2292263f9ffbfc7546064dd0e8d
Hamza Ehsan Khan <khanh29@cardiff.ac.uk>
2024-06-28 11:01:50 +0100

    define a Item schema beforehand in your items.py

--- free_code_camp_scrapy/bookscraper/bookscraper/items.py
+++ free_code_camp_scrapy/bookscraper/bookscraper/items.py
@@ -9,4 +9,16 @@ import scrapy
 class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    url = scrapy.Field()
+    title = scrapy.Field()
+    price = scrapy.Field()
+    description = scrapy.Field()
+    category = scrapy.Field()
+    stars = scrapy.Field()
+    upc = scrapy.Field()
+    product_type = scrapy.Field()
+    price_excl_tax = scrapy.Field()
+    price_incl_tax = scrapy.Field()
+    tax = scrapy.Field()
+    availability = scrapy.Field()
+    num_reviews = scrapy.Field()
